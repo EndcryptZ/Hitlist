19ff0ea87823641ad9c89b70bd46c4a657976201
Endcrypt <pternej32@gmail.com>
2025-08-20 10:41:07 +0800

    handle opt cooldown on player login/logout

--- src/main/java/com/endcrypt/hitlist/player/PlayerListener.java
+++ src/main/java/com/endcrypt/hitlist/player/PlayerListener.java
@@ -1,6 +1,7 @@
 package com.endcrypt.hitlist.player;
 
 import com.endcrypt.hitlist.HitlistPlugin;
+import com.endcrypt.hitlist.utils.ColorUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,12 +21,18 @@ public class PlayerListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         plugin.getPlayerManager().loadPlayer(event.getPlayer());
         plugin.getLogger().info("Loaded player data for " + event.getPlayer().getName());
+
+        handleOptCooldownOnJoin(event.getPlayer());
+
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
+        handleOptCooldownOnQuit(event.getPlayer());
+
         plugin.getPlayerManager().removePlayer(event.getPlayer());
         plugin.getLogger().info("Saved player data for " + event.getPlayer().getName() + " to disk.");
+
     }
 
     @EventHandler
@@ -40,5 +47,39 @@ public class PlayerListener implements Listener {
 
         plugin.getBountyManager().claimBounty(killer, event.getPlayer());
 
+    }
+
+    private void handleOptCooldownOnJoin(Player player) {
+        PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
+
+        boolean isOptInCooldownCountOnlineOnly = plugin.getConfigManager().getMain().isOptInCooldownCountOnlineOnly();
+        boolean isOptOutCooldownCountOnlineOnly = plugin.getConfigManager().getMain().isOptOutCooldownCountOnlineOnly();
+        boolean isPlayerOptedOut = playerData.isOptedOut();
+        long currentPlayerOptCooldown = playerData.getOptCooldown();
+        long lastPlayed = player.getLastPlayed();
+        long now = System.currentTimeMillis();
+        long timeElapsed = now - lastPlayed;
+        long newOptCooldown = (timeElapsed >= currentPlayerOptCooldown) ? 0 : currentPlayerOptCooldown - timeElapsed;
+
+        if (isPlayerOptedOut) {
+            if (!isOptOutCooldownCountOnlineOnly) {
+                plugin.getPlayerManager().getPlayerData(player).setOptCooldown(newOptCooldown);
+            }
+        } else {
+            if (!isOptInCooldownCountOnlineOnly) {
+                plugin.getPlayerManager().getPlayerData(player).setOptCooldown(newOptCooldown);
+            }
+        }
+    }
+
+    private void handleOptCooldownOnQuit(Player player) {
+        PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
+        long currentPlayerOptCooldown = playerData.getOptCooldown();
+        long lastLogin = player.getLastLogin();
+        long now = System.currentTimeMillis();
+        long timeElapsed = now - lastLogin;
+        long newOptCooldown = (timeElapsed >= currentPlayerOptCooldown) ? 0 : currentPlayerOptCooldown - timeElapsed;
+
+        plugin.getPlayerManager().getPlayerData(player).setOptCooldown(newOptCooldown);
     }
 }
